7a4fbbf8584ed9ce7e63bc33f5e84f36ff78184d
Javier Pastor <jpastor@cerebelum.net>
2022-02-13 00:35:11 +0100

    init 1.2

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,13 @@
+<?php
+/**
+*@package SMF 2.1 Fusion Theme 
+*@author Mick @idesignSMF.com
+*@copyright 2022 
+*@version 1.0
+**/
+$txt['facebook_url'] = 'Facebook URL';
+$txt['twitter_url'] = 'Twitter URL';
+$txt['youtube_url'] = 'YouTube URL';
+$txt['linkedin_url'] = 'LinkedIn URL';
+$txt['rss_url'] = 'RSS URL';
+?>
